421498cbc873db85f2a616c3ae402c3e4e457255
John Ubante <ubante@yahoo-inc.com>
2014-03-30 22:40:32 -0700

    Idea can make the oven repo but can't commit.  Getting this error:
    Successfully created project 'ove' on GitHub, but initial commit failed

---
+++ src/com/ubante/oven/TestClass.java
@@ -0,0 +1,27 @@
+package com.ubante.oven;
+
+/**
+ * Created by J on 3/30/2014.
+ */
+public class TestClass {
+    String aString = "string0";
+
+    TestClass() {}
+
+    
+
+    void printString() {
+        System.out.println(aString);
+    }
+
+    static void printSomething() {
+        System.out.println("something");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello hello");
+        printSomething();
+        TestClass tc = new TestClass();
+        tc.printString();
+    }
+}
