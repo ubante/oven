6b4403ec6584766c8521764160ac14f9c9f92ec3
John Ubante <ubante@yahoo-inc.com>
2014-04-22 23:31:14 -0700

    committing from laptop MBA

---
+++ src/main/java/com/ubante/oven/junk/TestingLaptopSync.java
@@ -0,0 +1,11 @@
+package com.ubante.oven.junk;
+
+/**
+ * This can be deleted when MBA commit is proven.
+ */
+public class TestingLaptopSync {
+
+    public static void main(String[] args) {
+        System.out.println("This originates from laptop 1");
+    }
+}
