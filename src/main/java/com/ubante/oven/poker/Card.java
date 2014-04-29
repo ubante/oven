5555f31d55f597bd9bf3501a63efb9f2ba7fa998
John Ubante <ubante@yahoo-inc.com>
2014-04-28 23:51:00 -0700

    poker day1

--- src/main/java/com/ubante/oven/poker/Card.java
+++ src/main/java/com/ubante/oven/poker/Card.java
@@ -1,9 +1,12 @@
 package com.ubante.oven.poker;
 
+import java.util.Random;
+
 /**
  * Created by J on 4/28/2014.
  */
 public class Card {
+    static int maxValue = 13; // This is an ace
     String suit;
     int value;
 
@@ -13,14 +16,36 @@ public class Card {
     }
 
     static Card getRandom() {
-        int v = 3;
-        String s = "a";
+        String suit = "";
+        Random r = new Random();
+        int value = r.nextInt(maxValue) + 2;
+        int suitNumber = r.nextInt(4); // 4 is the number of suits
 
-        return new Card(s,v);
+        switch (suitNumber) {
+            case 0:
+                suit = "S";
+                break;
+            case 1:
+                suit = "D";
+                break;
+            case 2:
+                suit = "H";
+                break;
+            case 3:
+                suit = "C";
+                break;
+        }
+
+        return new Card(suit,value);
+    }
+
+    @Override
+    public String toString() {
+        return suit+value;
     }
 
     void print() {
-        System.out.println(suit+value);
+        System.out.println(toString());
     }
 
     /**
@@ -31,6 +56,12 @@ public class Card {
         Card c = Card.getRandom();
 
         c.print();
+
+        for (int i=0; i<60; i++) {
+            c = Card.getRandom();
+            System.out.printf("%3s ",c.toString());
+        }
+        System.out.println();
     }
 
 }
---
+++ src/main/java/com/ubante/oven/poker/Suit.java
@@ -0,0 +1,8 @@
+package com.ubante.oven.poker;
+
+/**
+ * Created by J on 4/28/2014.
+ */
+public enum Suit {
+    SPADES,DIAMONDS,HEARTS,CLUBS;
+}
