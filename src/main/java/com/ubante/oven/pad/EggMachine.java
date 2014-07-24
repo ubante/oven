089321ab74a152f8de6b0c4afb1a3389e18b10b3
ubante <ubante@yahoo-inc.com>
2014-07-24 15:44:16 -0700

    Merging

--- src/main/java/com/ubante/oven/pad/EggMachine.java
+++ src/main/java/com/ubante/oven/pad/EggMachine.java
@@ -2,25 +2,48 @@ package com.ubante.oven.pad;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
- * Created by J on 7/21/2014.
+ * It's an egg machine.
  */
 public class EggMachine {
-    List<Monster> monsterList = new ArrayList();
+    List<Monster> monsterList = new ArrayList<>();
+    int monsterListSize;
+    int pullCounter = 0;
+    Random r = new Random();
 
-    EggMachine() {}
+    EggMachine(int monsterListSize) {
+        this.monsterListSize = monsterListSize;
+        for (int i=0; i<monsterListSize; i++) {
+            monsterList.add(new Monster("a"+i));
+        }
+    }
+
+    void printMonsterList() {
+        System.out.println("Here's the list of monsters:");
+        for (Monster m : monsterList) {
+            System.out.printf("%s, ", m.name);
+        }
+        System.out.println();
+    }
 
     Monster pull() {
-        Monster m = new Monster();
+        int index = r.nextInt(monsterListSize);
 
-        return m;
+//        System.out.println("\nUsing index=" + index);
+
+        pullCounter++;
+
+        return monsterList.get(index);
     }
 
     public static void main(String[] args) {
-        EggMachine bem = new EggMachine();
+        EggMachine bem = new EggMachine(5);
+        Collection c = new Collection(5);
 
-        Monster pulled = bem.pull();
+        bem.printMonsterList();
+
 
     }
 
