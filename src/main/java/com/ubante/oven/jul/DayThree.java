e36e3e71b4b3209d92b6ab41a14d1e064dce8e5b
ubante <ubante@yahoo-inc.com>
2014-07-30 17:08:09 -0700

    turning off logging to console

--- src/main/java/com/ubante/oven/jul/DayThree.java
+++ src/main/java/com/ubante/oven/jul/DayThree.java
@@ -11,22 +11,11 @@ public class DayThree {
     private static Logger theLogger = Logger.getLogger(DayThree.class.getName());
     String message;
 
-    DayThree(String s) { message = s; }
-
-    void printString() {
-        System.out.println(message);
-    }
-
-    public static void main(String[] args) {
-        String logPath = "/home/ubante/tmp/theloggerLogs/Day3.log";
-
-        DayThree ex = new DayThree("Some string to log");
-        ex.printString();
-
+    DayThree(String s) {
         FileHandler fh;
-
+        String logPath = "/home/ubante/tmp/theloggerLogs/Day3.log";
         try {
-            fh = new FileHandler(logPath);
+            fh = new FileHandler(logPath, true);
             theLogger.addHandler(fh);
             SimpleFormatter formatter = new SimpleFormatter();
             fh.setFormatter(formatter);
@@ -34,12 +23,48 @@ public class DayThree {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        message = s;
+    }
+
+    void printString() {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+
+        DayThree ex = new DayThree("Some string to log");
+        ex.printString();
+
+
+        // disable logging to the console
+//        theLogger.setUseParentHandlers(false);
+
+        // since I don't want to do this in Cat
+        Logger globalLogger = Logger.getLogger("");
+        Handler[] handlers = globalLogger.getHandlers();
+        for (Handler handler : handlers) {
+            globalLogger.removeHandler(handler);
+        }
+
+//        FileHandler fh;
+//        String logPath = "/home/ubante/tmp/theloggerLogs/Day3.log";
+//        try {
+//            fh = new FileHandler(logPath, true);
+//            theLogger.addHandler(fh);
+//            SimpleFormatter formatter = new SimpleFormatter();
+//            fh.setFormatter(formatter);
+//            theLogger.info("test 123");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
 
 
         // turn up the Logger object and its handlers
         theLogger.setLevel(Level.FINE);
-        Handler[] handlers = Logger.getLogger("").getHandlers();
-        for (Handler handler : handlers) {
+        Handler[] handlers2 = Logger.getLogger("").getHandlers();
+        for (Handler handler : handlers2) {
             System.out.println("Adjusting to FINER: " + handler.toString());
             handler.setLevel(Level.FINER);
         }
